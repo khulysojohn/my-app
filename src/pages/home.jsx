8a387fbad05801431818ee767d3f6f110e498d5b
khulyso <khulyso.dev@gmail.com>
2024-10-14 10:26:55 +0200

    new update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -23,11 +23,11 @@ const Home = () => {
 
   return (
     <div id="home-container" className="w-full h-screen flex flex-col pt-40 pb-32 bg-[#8d450c] md:flex-row gap-2">
-      <div id="text-content" className="mx-4 px-8 mt-10 text-orange-200 justify-center">
+      <div id="text-content" className="mx-4 px-8 mt-10 pb-8 h-fit text-orange-200 justify-center">
         <p className="mx-auto text-xl">Hello, and welcome to </p>
-        <h1 id="name" className="text-2xl sm:text-7xl mx-auto font-bold">Fountain of Fire</h1>
+        <h1 id="name" className="text-2xl sm:text-7xl py-2 mx-auto font-bold">Fountain of Fire</h1>
         <h2 id="surname" className="text-2xl sm:text-7xl mx-auto font-bold">Ministry</h2>
-        <p className="py-4 max-w-[700px]">
+        <p className="py-8 max-w-[700px]">
           We are glad you're here. Whether you're exploring faith or looking for a church to call home, we are excited to share 
           the love of Christ with you. Come as you are, and experience a community that loves to pray and grow together.
         </p>
@@ -43,7 +43,10 @@ const Home = () => {
           </a>
         </div>
       </div>
-      <img id="img" src={img} className="px-8 mb-6" max-width="70%" alt="home" />
+      <div className='w-50%'>
+      <img id="img" src={img} className="px-8 mb-6" width="85%" alt="home" />
+      </div>
+     
     </div>
   );
 };
